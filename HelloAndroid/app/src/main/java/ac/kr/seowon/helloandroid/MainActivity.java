a3ab8bf8e7e225be7b504ff60fb005629f45c692
seokjh09 <juhee1757@gmail.com>
2022-04-15 16:47:31 +0900

    first commit

---
+++ HelloAndroid/app/src/main/java/ac/kr/seowon/helloandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package ac.kr.seowon.helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
